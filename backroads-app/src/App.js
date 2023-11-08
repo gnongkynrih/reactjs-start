ab88349698d0f4725a02a28daeacf6443324aa74
gnongkynrih <kynsai@gmail.com>
2023-11-08 17:04:41 +0530

    first

---
+++ backroads-app/src/App.js
@@ -0,0 +1,24 @@
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+
+      <About />
+
+      <Services />
+
+      <Tours />
+
+      <Footer />
+    </>
+  );
+}
+export default App;
