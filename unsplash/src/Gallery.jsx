920714e0a719d4408d37da1e7788a9f703dd89ed
gnongkynrih <kynsai@gmail.com>
2023-11-11 15:36:08 +0530

    mix master

--- unsplash/src/Gallery.jsx
+++ unsplash/src/Gallery.jsx
@@ -1,45 +1,45 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import { useGlobalContext } from './context';
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useGlobalContext } from "./context";
 
 const url = `https://api.unsplash.com/search/photos?client_id=${
   import.meta.env.VITE_API_KEY
 }`;
 const Gallery = () => {
   const { searchTerm } = useGlobalContext();
-  const response = useQuery({
-    queryKey: ['images', searchTerm],
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["images", searchTerm],
     queryFn: async () => {
       const result = await axios.get(`${url}&query=${searchTerm}`);
       return result.data;
     },
   });
-  if (response.isLoading) {
+  if (isLoading) {
     return (
-      <section className='image-container'>
+      <section className="image-container">
         <h4>Loading...</h4>
       </section>
     );
   }
-  if (response.isError) {
+  if (isError) {
     return (
-      <section className='image-container'>
+      <section className="image-container">
         <h4>There was an error...</h4>
       </section>
     );
   }
 
-  const results = response.data.results;
+  const results = data.results;
   if (results.length < 1) {
     return (
-      <section className='image-container'>
+      <section className="image-container">
         <h4>No results found...</h4>
       </section>
     );
   }
 
   return (
-    <section className='image-container'>
+    <section className="image-container">
       {results.map((item) => {
         const url = item?.urls?.regular;
         return (
@@ -47,7 +47,7 @@ const Gallery = () => {
             src={url}
             key={item.id}
             alt={item.alt_description}
-            className='img'
+            className="img"
           ></img>
         );
       })}
